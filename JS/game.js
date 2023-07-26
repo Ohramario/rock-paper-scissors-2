0afa00172fad3d18d1bee08e4fefad50212a4fc4
Kris <93468625+Ohramario@users.noreply.github.com>
2023-07-26 19:22:57 +0300

    I have improved a wee bit

--- JS/game.js
+++ JS/game.js
@@ -1,6 +1,14 @@
+
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
+
+
+
+let playerScore = 0;
+let computerScore = 0;
+let runningScore = "player: " + playerScore + ", computer: " + computerScore;
+
 
 
 
@@ -25,23 +33,36 @@ function playRound(playerSelection, computerSelection) {
 
     let playerString = playerSelection;
 
+    let computerString = computerSelection;
+
     /* evaluates win conditions */
 
 
-    const conditions = [playerString == rock && computerSelection == scissors, playerString == scissors && computerSelection == paper, playerString == paper && computerString == rock];
+    const conditions = [playerString == rock && computerString == scissors, playerString == scissors && computerString == paper, playerString == paper && computerString == rock];
 
     const allConditionsMet = conditions.every(condition => condition);
 
 
+
+
+
+
     if (allConditionsMet) {
 
-        let winMessage = "Player Wins !"
-        return winMessage;
+        increment("player");
+
+
+        if (playerScore == 5) {
+            return "player wins"
+        } else {
+            return runningScore;
+        }
+
     }
 
 
     /* evaluates and returns draw condition */
-    else if (playerString == computerSelection) {
+    else if (playerString == computerString) {
 
         let drawMessage = "It is a draw !";
 
@@ -52,24 +73,33 @@ function playRound(playerSelection, computerSelection) {
     /*evaluates and returns loss condition */
 
 
-
     else {
 
-        let lossMessage = "Player loses !";
+        increment("computer");
 
-        return lossMessage;
+        if (computerScore == 5) {
+            return "computer wins!"
+        } else {
+            return "player: " + playerScore + ", computer: " + computerScore;
+
+        }
+
+    }
+}
+
+function increment(variable) {
+    if (variable == "player") {
+        playerScore++;
+    } else if (variable == "computer") {
+        computerScore++;
     }
 }
 
 
-function game() {
 
-    let playerInput = prompt("Please input one of the following: rock, paper, scissors");
+function game(input) {
 
-    let choice = playerInput.toLowerCase();
-
-
-    let playerSelection = checkValidity(choice);
+    let playerSelection = input;
 
 
     let computerSelection = getComputerChoice();
@@ -80,28 +110,17 @@ function game() {
 }
 
 
-/* function checks if valid player input */
-function checkValidity(input) {
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
 
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
 
-
-    if (input == rock || input == scissors || input == paper) {
-
-        return input;
-
-
-    } else {
-
-
-        let againMessage = "Not valid, please refresh page";
-
-        alert(againMessage);
-    }
-
-}
-
-game();
-
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        game(button.id);
+    });
+});
 
 
 
